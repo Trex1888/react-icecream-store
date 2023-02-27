76c1d0e7e2d9d33dcc87855afa5922adbaba0bc4
Harpua <harpua123@gmail.com>
2023-02-26 19:41:33 -0600

    1st commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,55 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+// it("should render out row 1", async () => {
+//   render(<App />);
+//   const getRow1 = await screen.findByRole("row", {
+//     name: /oreos 7 10/i,
+//   });
+//   expect(getRow1).toBeVisible();
+// });
+
+// it("should subtract 1 from quality and sellin", async () => {
+//   render(<App />);
+//   const subClick = await screen.findByRole("button", {
+//     name: "Sub",
+//   });
+//   userEvent.click(subClick);
+
+//   const newRow = await screen.findByRole("row", {
+//     name: "Oreos 6 9",
+//   });
+//   expect(newRow).toBeInTheDocument();
+// });
+
+// it("should subtract 2 from quality if sellin = 0", async () => {
+//   render(<App />);
+//   const subClick = await screen.findByRole("button", {
+//     name: "Sub",
+//   });
+//   userEvent.click(subClick);
+//   userEvent.click(subClick);
+//   userEvent.click(subClick);
+//   userEvent.click(subClick);
+//   userEvent.click(subClick);
+//   userEvent.click(subClick);
+
+//   const newRow = await screen.findByRole("row", {
+//     name: "Corn -4 0",
+//   });
+//   expect(newRow).toBeInTheDocument();
+// });
+
+it("should add 1 to cart and $5 to total", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const addClick = await screen.findByRole("button", {
+    name: "Add",
+  });
+  userEvent.click(addClick);
+
+  const newtotal = await screen.findByText("Shopping Cart: 1 total items");
+  const newTotal = await screen.findByText("Total: 5.00");
+  expect(newtotal).toBeInTheDocument();
+  expect(newTotal).toBeTruthy();
 });
